75750fe4fff2d31a346861d56158d6a6e80f000d
PengHebe <PengHebe@qq.com>
2015-09-10 00:33:19 +0800

    add App

--- hebe-demo-netty-scala/build.sbt
+++
@@ -1,8 +0,0 @@
-organization := "com.example"
-
-name := "hello"
-
-version := "0.1.0"
-
-scalaVersion := "2.11.4"
-
---
+++ hebe-demo-netty/src/main/java/com/peng/hebe/demo/netty/App.java
@@ -0,0 +1,10 @@
+package com.peng.hebe.demo.netty;
+
+/**
+ * Created by peng on 15-9-10.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
